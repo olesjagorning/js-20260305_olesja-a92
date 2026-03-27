f8d7f6f29e209866491b0cef26468ab7aa36bf3e
Olesia Gorning <olesja.a92@gmail.com>
2026-03-27 13:56:45 +0300

    5 урок - NotificationMessage и SortableTable v1

--- 05-dom-document-loading/1-notification/index.ts
+++ 05-dom-document-loading/1-notification/index.ts
@@ -1,9 +1,62 @@
 import { createElement } from "../../shared/utils/create-element";
 
-interface Options { }
+interface Options {
+  duration: number;
+  type:'success' | 'error';
+}
 
 export default class NotificationMessage {
+  static activeNotification: NotificationMessage | null;
+  public element: HTMLElement | null;
+  private timerId: number = 0;
 
-  constructor(message: string, { }: Options = {}) {
+  constructor(private message: string, private options: Options = {'duration': 2000, type: 'success'}) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+    this.element = createElement(this.template);
+    NotificationMessage.activeNotification = this;
+  }
+
+  private get template() {
+    return `<div class="notification ${this.options.type}" style="--value:${this.options.duration/1000}s">
+              <div class="timer"></div>
+              <div class="inner-wrapper">
+                <div class="notification-header">${this.options.type}</div>
+                <div class="notification-body">
+                  ${this.message}
+                </div>
+              </div>
+            </div>`;
+  }
+
+  show(target?: HTMLElement){
+    if(!this.element) return;
+
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+    target = target ?? document.body;
+    NotificationMessage.activeNotification = this;
+
+    target.append(this.element);
+
+    this.timerId = setTimeout(()=>{
+       this.remove();
+    }, this.options.duration);
+  }
+
+  public remove() {
+    if(!this.element) return;
+    this.element.remove();
+  }
+
+  public destroy() {
+    this.remove();
+    if (NotificationMessage.activeNotification === this) {
+      NotificationMessage.activeNotification = null;
+    }
+    this.element = null;
+    clearTimeout(this.timerId);
   }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.ts
+++ 05-dom-document-loading/2-sortable-table-v1/index.ts
@@ -1,3 +1,5 @@
+import {createElement} from "../../shared/utils/create-element";
+
 type SortOrder = 'asc' | 'desc';
 
 type SortableTableData = Record<string, string | number>;
@@ -11,6 +13,93 @@ interface SortableTableHeader {
 }
 
 export default class SortableTable {
-  constructor(headersConfig: SortableTableHeader[] = [], data: SortableTableData[] = []) {
+  public element: HTMLElement | null;
+  public bodyElement: HTMLDivElement | null;
+  public headerElement: HTMLDivElement | null;
+
+  constructor(private headersConfig: SortableTableHeader[] = [],private data: SortableTableData[] = []) {
+    this.element = createElement(this.template);
+    this.bodyElement =  this.element.querySelector<HTMLDivElement>('[data-element="body"]');
+    this.headerElement =  this.element.querySelector<HTMLDivElement>('[data-element="header"]');
+  }
+
+  private get template() {
+    const headerContent =  this.renderHeader();
+    const bodyContent = this.renderRows();
+
+    return `<div class="sortable-table">
+                <div data-element="header" class="sortable-table__header sortable-table__row">
+                  ${headerContent}
+                </div>
+                <div data-element="body" class="sortable-table__body">
+                       ${bodyContent}
+                </div>
+                <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+                <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+                   <div>
+                     <p>No products satisfies your filter criteria</p>
+                     <button type="button" class="button-primary-outline">Reset all filters</button>
+                   </div>
+                </div>
+            </div>`;
+  }
+
+  private renderRows(){
+    return this.data?.map((dataItem)=>{
+      const columnItem =  this.headersConfig?.map((configItem)=>{
+        if(dataItem[configItem.id]) {
+          return configItem.template?
+                  configItem.template(dataItem[configItem.id]):
+                  `<div class="sortable-table__cell">${dataItem[configItem.id]}</div>`;
+        }
+      }).join('');
+
+      return `<a href="/products/${dataItem.id}" class="sortable-table__row">${columnItem}</a>`
+    }).join('');
+  }
+
+  private renderHeader(){
+    return this.headersConfig?.map((configItem)=>{
+      return `<div
+                class="sortable-table__cell"
+                data-id="${configItem.id}"
+                data-sortable="${configItem.sortable??false}">
+                    ${configItem.title}
+              </div>`;
+    }).join('');
+  }
+
+  public sort(field: string, order: SortOrder){
+      if(!this.element) return;
+      if(!this.bodyElement) return;
+      if(!this.headerElement) return;
+
+      const directions = { asc: 1, desc: -1 };
+      const column = this.headersConfig.find(item => item.id === field && item.sortable === true);
+      if(!column) return;
+
+      this.data.sort((rowA,rowB) => {
+        if(typeof rowA[field] === 'undefined' || typeof rowB[field]  === 'undefined') return 0;
+
+        if(typeof rowA[field] === 'string' && typeof rowB[field] === 'string') {
+          return (rowA[field] as string).localeCompare((rowB[field] as string), ["ru", "en"], {caseFirst: "upper"}) * directions[order];
+        } else if (typeof rowA[field] === 'number' && typeof rowB[field] === 'number'){
+          return (rowA[field]-rowB[field]) * directions[order];
+        } else return 0;
+      });
+
+      this.bodyElement.innerHTML = this.renderRows();
+      this.headerElement.querySelectorAll('[data-order]')?.forEach(el => el.removeAttribute('data-order'))
+      this.headerElement.querySelector<HTMLDivElement>(`[data-id="${field}"]`)?.setAttribute('data-order',order);
+  }
+
+  public remove(){
+    if(!this.element) return;
+    this.element.remove();
+  }
+
+  public destroy(){
+    this.remove();
+    this.element = null;
   }
 }
